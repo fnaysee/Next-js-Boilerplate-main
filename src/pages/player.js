7836f6bdc0086dbce2a7c795ff84c6faf86af570
wp-src <wpmagic70@gmail.com>
2023-01-01 13:37:18 +0330

    Update

--- src/pages/player.js
+++ src/pages/player.js
@@ -2,27 +2,48 @@ import 'pod-comm-offline-sdk/dist/node/css/style.css';
 import 'pod-comm-offline-sdk/dist/node/css/material-icons.min.css';
 
 let SDK;
+let isAppendedPlayer = false;
 
 function Player() {
+  const myInterval = setInterval(async () => {
+    if (
+      !isAppendedPlayer &&
+      typeof window !== 'undefined' &&
+      document.getElementById('player-root') &&
+      !document.getElementById('stream-video-container')
+    ) {
+      isAppendedPlayer = true;
+      const OfflinePlayer = (await import('pod-comm-offline-sdk')).default;
+      SDK = new OfflinePlayer({
+        containerId: 'player-root',
+        env: 'main',
+        logging: {
+          info: true,
+          debug: true,
+          error: true,
+        },
+        showForwardBackwardButtons: true,
+        fullScreenPlayer: false,
+        loadAssetsFromCDN: false,
+      });
+      try {
+        SDK.playMedia({
+          hash: 'QS6TQ7A1FV3YOK22', // '8FEBGXQM25Q3GRHZ',//"DBPPGRD3PUPV4MNL",
+          quality: 240,
+          token: 'ed24e37c7ee84313acf2805a80122f94',
+        });
+      } catch (error) {
+        console.error({ error });
+      }
+
+      clearInterval(myInterval);
+    }
+  }, 2000);
   return (
-    <div id="playerroot">
+    <div id="player-root">
       <button
         onClick={async (e) => {
           // Dynamically load fuse.js
-          const OfflinePlayer = (await import('pod-comm-offline-sdk')).default;
-
-          SDK = new OfflinePlayer({
-            containerId: 'playerroot',
-            env: 'main',
-            logging: {
-              info: true,
-              debug: true,
-              error: true,
-            },
-            showForwardBackwardButtons: true,
-            fullScreenPlayer: false,
-            loadAssetsFromCDN: false,
-          });
 
           SDK.playMedia({
             hash: 'QS6TQ7A1FV3YOK22', // '8FEBGXQM25Q3GRHZ',//"DBPPGRD3PUPV4MNL",
